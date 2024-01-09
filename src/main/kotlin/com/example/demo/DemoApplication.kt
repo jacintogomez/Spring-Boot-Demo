9e36a2c8ac186627040d8fd0fd073f05ff45b46b
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-01-08 19:26:50 -0500

    added H2 database

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -5,6 +5,26 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Service
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PostMapping
+import java.util.UUID
+
+@Service
+class MessageService(val db: JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun save(message: Message) {
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update(
+            "insert into messages values ( ?, ? )",
+            id, message.text
+        )
+    }
+}
 
 @SpringBootApplication
 class DemoApplication
@@ -14,13 +34,14 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageController {
+class MessageController(val service: MessageService) {
     @GetMapping("/")
-    fun index() = listOf(
-        Message("1", "Hello!"),
-        Message("2", "Bonjour!"),
-        Message("3", "Privet!"),
-    )
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
 }
 
 data class Message(val id: String?, val text: String)
